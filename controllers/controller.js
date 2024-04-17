7b9de090203ba4c9a43a75b2cb5f166413b0a989
Martin Leite Jensen <marekatt2004@gmail.com>
2024-04-17 21:07:33 +0200

    added page for ships

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,7 +10,19 @@ const get_characters = async (req, res) => {
 }
 
 
+const get_fleet = async (req, res) => {
+    const playerResponse = await fetch("https://swgoh.gg/api/player/471196449")
+    const playerData = await playerResponse.json()
+    let sortedPlayerData = playerData.units.sort((char1, char2) => (char1.data.power < char2.data.power) ? 1 : (char1.data.power > char2.data.power) ? -1 : 0);
+    
+    const charactersResponse = await fetch("https://swgoh.gg/api/characters")
+    const charactersData = await charactersResponse.json()
+
+    res.render("fleet", { player: sortedPlayerData, characters: charactersData })
+}
+
 
 module.exports = {
-    get_characters
+    get_characters,
+    get_fleet
 }
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { get_characters } = require("../controllers/controller");
+const { get_characters, get_fleet } = require("../controllers/controller");
 
 const router = Router();
 
 router.get("/", get_characters)
+router.get("/fleet", get_fleet)
 
 module.exports = router
